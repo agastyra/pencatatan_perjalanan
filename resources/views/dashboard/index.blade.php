2e637f37966241e68239f479d0f2e80c639db66b
agastyra <rangga.agastya711@gmail.com>
2022-02-24 20:25:46 +0700

    menambahkan fitur authorization

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,7 +3,11 @@
 @section('container')
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
         <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 class="h2">Daftar perjalanan kamu</h1>
+            @if (auth()->user()->id_level !== 3)
+                <h1 class="h2">Daftar perjalanan</h1>
+            @else
+                <h1 class="h2">Daftar perjalanan kamu</h1>
+            @endif
         </div>
 
         @if (session()->has('success'))
@@ -23,14 +27,25 @@
             <div class="col-lg-6">
                 <form action="/dashboard" method="GET">
                     <div class="input-group mb-3">
-                        <input type="text" class="form-control @error('q') is-invalid @enderror"
-                            placeholder="Tujuan atau keperluan..." id="q" name="q" value="{{ request('q') }}"
-                            autocomplete="off" autofocus>
-                        @error('q')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                        @enderror
+                        @if (auth()->user()->id_level !== 3)
+                            <input type="text" class="form-control @error('q') is-invalid @enderror"
+                                placeholder="Cari berdasarkan NIK, Nama, Tujuan, atau Keperluan..." id="q" name="q"
+                                value="{{ request('q') }}" autocomplete="off" autofocus>
+                            @error('q')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        @else
+                            <input type="text" class="form-control @error('q') is-invalid @enderror"
+                                placeholder="Cari berdasarkan Tujuan atau Keperluan..." id="q" name="q"
+                                value="{{ request('q') }}" autocomplete="off" autofocus>
+                            @error('q')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        @endif
                         <button class="btn btn-outline-secondary" type="submit">Cari</button>
                     </div>
                 </form>
@@ -40,27 +55,55 @@
         @if ($perjalanans->count() > 0)
             <div class="table-responsive mb-5">
                 <table class="table table-striped table-sm">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Tanggal</th>
-                            <th scope="col">Suhu tubuh</th>
-                            <th scope="col">Tujuan</th>
-                            <th scope="col">Keperluan</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($perjalanans as $perjalanan)
+                    @if (auth()->user()->id_level !== 3)
+                        <thead>
                             <tr>
-                                <td>{{ $loop->iteration }}</td>
-                                <td>{{ $perjalanan->tanggal }}</td>
-                                <td>{{ $perjalanan->suhu_tubuh }}</td>
-                                <td>{{ $perjalanan->tujuan }}</td>
-                                <td>{{ $perjalanan->keperluan }}</td>
-                                <td></td>
+                                <th scope="col">#</th>
+                                <th scope="col">Tanggal</th>
+                                <th scope="col">NIK</th>
+                                <th scope="col">Nama</th>
+                                <th scope="col">Suhu tubuh</th>
+                                <th scope="col">Tujuan</th>
+                                <th scope="col">Keperluan</th>
                             </tr>
-                        @endforeach
-                    </tbody>
+                        </thead>
+                        <tbody>
+                            @forelse ($perjalanans as $perjalanan)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $perjalanan->tanggal }}</td>
+                                    <td>{{ $perjalanan->nik }}</td>
+                                    <td>{{ $perjalanan->nama }}</td>
+                                    <td>{{ $perjalanan->suhu_tubuh }}</td>
+                                    <td>{{ $perjalanan->tujuan }}</td>
+                                    <td>{{ $perjalanan->keperluan }}</td>
+                                    <td></td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    @else
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Tanggal</th>
+                                <th scope="col">Suhu tubuh</th>
+                                <th scope="col">Tujuan</th>
+                                <th scope="col">Keperluan</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($perjalanans as $perjalanan)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $perjalanan->tanggal }}</td>
+                                    <td>{{ $perjalanan->suhu_tubuh }}</td>
+                                    <td>{{ $perjalanan->tujuan }}</td>
+                                    <td>{{ $perjalanan->keperluan }}</td>
+                                    <td></td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    @endif
                 </table>
             @else
                 <h6 class="text-center text-muted my-3">Tidak ada perjalanan</h6>
